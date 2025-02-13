9a5d820c10183306ccb096ff4a268c6fd39f2e47
Ngo Quang Duc <ngoquangduc@MacBook-Air-cua-Hoang.local>
2025-02-13 22:35:57 +0700

    add more pop up UI

--- frontend/app.py
+++ frontend/app.py
@@ -126,20 +126,8 @@ if st.session_state.embeddings_done:
         # 🔍 Embed khuôn mặt từ ảnh CCCD
         status = st.empty()  # 👈 Tạo placeholder để xóa trạng thái sau khi chạy xong
         status.write("🧑‍💻 **Đang tạo embeddings từ ảnh CCCD...**")
-        
-        result = embed_face_cccd(face_model, facenet_model, image)
-        st.write(f"🔍 DEBUG: Kết quả embed_face_cccd: {result}")  # ✅ Debug
-
-        if result is None:
-            st.error("❌ Lỗi hệ thống: `embed_face_cccd()` không trả về dữ liệu!")
-            st.stop()
-
-
-        face_embedding_cccd, error_message = embed_face_cccd(face_model, facenet_model, image_np)
+        face_embedding_cccd, error = embed_face_cccd(face_model, facenet_model, image_np)
         # status.empty()  # ❌ Xóa dòng trạng thái
-        if error_message:
-            st.error(error_message)  # Hiển thị lỗi trên UI
-            st.stop()  # Dừng xử lý tiếp theo
         
         user_embeddings = get_embeddings_from_firestore(user_id)
         if user_embeddings is None:
